a873e821607ca1d826390d61902ff76420df4b6a
Martyn Eggleton <martyn.eggleton@gmail.com>
2012-05-11 14:22:01 +0100

    Adds the scons parameters needed

--- run.php
+++ run.php
@@ -1,15 +1,26 @@
 <?php
 
 $sScriptFile = "output.pde";
-$sOutputFolder = "output/";
+$sOutputFolder = "/tmp/output/";
+
+if(!file_exists($sOutputFolder))
+{
+	mkdir($sOutputFolder);
+	copy("external/arscons/SConstruct", $sOutputFolder."SConstruct");
+
+}
+
+
 
 $sScript = false;
 if(isset($_POST['script']))
 {
   $sScript = $_POST['script'];
+  //echo $sScript ."\n";
   file_put_contents($sOutputFolder.$sScriptFile, $sScript);
-  
-  $sOutput = shell_exec("cd $sOutputFolder; scons ; scons upload");
+  $sCMD = "cd $sOutputFolder;echo \"typhoon\" | sudo -u root -S scons upload ARDUINO_BOARD=uno  ARDUINO_PORT=/dev/ttyACM0  ";//EXTRA_LIB=/home/tech/ebl-arduino/";
+  echo $sCMD."\n";
+  $sOutput = system($sCMD);
   
   echo $sOutput;
   file_put_contents($sOutputFolder.'output.log', $sOutput);
