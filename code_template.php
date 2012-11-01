f06be096527804c1da87558c8fc244fc30b14a2e
Martyn Eggleton <martyn.eggleton@gmail.com>
2012-11-01 17:04:23 +0000

    only sends the templates for the current plugin type.

--- code_template.php
+++ code_template.php
@@ -1,6 +1,8 @@
 <?php
 $sBase = 'templates/';
 
+$sType = $_REQUEST['type'];
+
 //Should be genereated by a folder scan
 $aTemplateNames = array(
 	'ardumoto' => 'arduino-simple-robot/ardumoto.pde',
@@ -13,12 +15,17 @@ $aTemplates = array();
 
 foreach($aTemplateNames as $sName => $sFile)
 {
-	$sFullFile = $sBase.$sFile;
-	
-	if(file_exists($sFullFile))
-	{
-		$aTemplates[$sName] = file_get_contents($sFullFile);
-	}
+  if (is_null($sType) || $sType == dirname($sFile))
+  {
+    $sFullFile = $sBase.$sFile;
+    
+    if(file_exists($sFullFile))
+    {
+      $aTemplates[$sName] = file_get_contents($sFullFile);
+    }
+  }
 }
+
 $sRet = json_encode($aTemplates);
+
 echo $sRet;
